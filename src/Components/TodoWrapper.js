68c5ca44e985134e96ef7145c53d04f48579dfc6
Rushikesh <rushikeshdasarwad23@gmail.com>
2023-12-15 20:55:19 +0530

    Updated Favicon

--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -46,7 +46,7 @@ const TodoWrapper = () => {
 
   return (
     <div className="TodoWrapper">
-      <h1>Get Things Done!!</h1>
+      <h1>Todo List</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) =>
         todo.isEditing ? (
